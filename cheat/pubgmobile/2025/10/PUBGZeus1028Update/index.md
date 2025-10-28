bf01d7cd8f4b6a5d77ffa133d032935980ca02ec
MingYuePop <bushrakaneez5@gmail.com>
2025-10-28 14:29:52 +0800

    更新新的作弊内容

---
+++ cheat/pubgmobile/2025/10/PUBGZeus1028Update/index.md
@@ -0,0 +1,23 @@
+---
+title: PUBG宙斯ZEUS10.28更新
+description: ZEUS宙斯PUBGM公益版本，修复自瞄功能，包含辅助文件和使用说明。
+keywords:
+  - PUBG移动端
+  - ZEUS宙斯
+  - 公益版本
+  - 自瞄修复
+  - 游戏辅助
+---
+
+# PUBG宙斯ZEUS10.28更新
+
+## 介绍
+ZEUS宙斯PUBGM公益版本，本次更新修复了自瞄功能。包含完整的辅助文件和使用说明，如遇到卡屏问题可长按音量+退出程序。
+
+![PUBG宙斯ZEUS10.28更新](image.png)
+## 下载
+
+| 下载 | 网盘链接 | 提取码 |
+| ---- | -------- | ------------ |
+| 夸克网盘 | https://pan.quark.cn/s/c29654209ccb |  |
+| 移动云盘 | https://caiyun.139.com/w/i/2qidYCbEZWTjv | topc |
--- sidebarsCheat.js
+++ sidebarsCheat.js
@@ -15,6 +15,10 @@ const sidebars = {
           type: 'category',
           label: '2025/10',
           items: [
+            'hpjy/2025/10/PeaceDoraemonCrack/index',
+            'hpjy/2025/10/PeaceEliteEggplantKernelCrack/index',
+            'hpjy/2025/10/PeaceEliteSssGyroLeakHitChaseLockCrack/index',
+            'hpjy/2025/10/PeaceEliteIceDragonContainerDirectInstall1028Crack/index',
             'hpjy/2025/10/FSKernelPlus2.1/index',
             'hpjy/2025/10/YushengHepingPublicWelfareReformDocument/index',
             'hpjy/2025/10/REandPeaceFilePublicWelfarePAKV11/index',    // 创建文档
@@ -28,6 +32,14 @@ const sidebars = {
       label: 'PUBG移动端',
       items: [
         'pubgmobile/intro',    // 创建文档
+        // 2025年10月
+        {
+          type: 'category',
+          label: '2025/10',
+          items: [
+            'pubgmobile/2025/10/PUBGZeus1028Update/index',
+          ],
+        },
       ],
     },
     // 其他游戏
@@ -36,6 +48,9 @@ const sidebars = {
       label: '其他游戏',
       items: [
         'other/intro',
+        'other/HonorOfKingsRazerPureAlgorithmDecryption103/index',
+        'other/HonorOfKingsFallingStarBeautify/index',
+        'other/KalabichiuZeroXPublicWelfareKernel/index',
         'other/LOLLEOPublicWelfareKernel/index',
         'other/GrassStealingNotesBuiltinMODMenu/index',        
         'other/KoreanFantasy/index',        
